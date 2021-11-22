4cf8919bfe51facd991e764f09ed0a9447b2c989
ferhattoufik@outlook.fr <toufik@MLA.bx0ii1vp1epulgqdspm020245b.fx.internal.cloudapp.net>
2021-11-22 18:59:54 +0000

    Bug fix on black box model, added some methods to store train history, wieghts and confusions matrix

--- README.md
+++ README.md
@@ -13,14 +13,24 @@ The dataset is the GTZAN music genre collection composed of **1000** music files
 The dataset is pre-processed in the following way :
 * For each music file, the spectrogram is extracted from the audio file using the [librosa](https://librosa.github.io/librosa/) library.
 * The spectrogram is resized to a fixed size using the tensorflow.keras.preprocessing.image.ImageDataGenerator.flow_from_directory.
-* Data splits into test and train are performed using the tensorflow.keras.preprocessing.image.ImageDataGenerator().
-* Data are saved as TFRecord 
+* Data splits into test and train are performed using spltit_dataset.py script.
 
-You can download the processed data from:
-* Link : https://mega.nz/folder/emhlSaBI
-* Key : BGdnOdVRIe10y54EUXztGw
 
-## Model
+## Models
+The models are implemented using the [tensorflow.keras](https://www.tensorflow.org/api_docs/python/tf/keras) library.
+We use the folowing models :
+### Black Box Model 
+The black box model is a convolutional neural network with a fixed architecture inspired by the [SPECTRAL CONVOLUTIONAL NEURAL NETWORK FOR 
+MUSIC CLASSIFICATION](https://publik.tuwien.ac.at/files/publik_255986.pdf) architecture and improved by Experimenting with [Musically Motivated Convolutional Neural Networks](http://jordipons.me/media/CBMI16.pdf)
+
+The architecture is defined in the [black_box_mmodel](model_architecture.py) file and it is as follows :
+* The input layer is a 2D convolutional layer with 32 filters, a kernel size of 12x8
+* A max pooling layer is added after the convolutional layer of size 4x1
+* The convolutional layer is flattened and connected to a dense layer with 200 neurons
+* Finally, the dense layer is connected to a softmax layer with 10 neurons to output a 10-dimensional vector
+
+
+
 
 ## Results
 
--- black_box_model.py
+++ black_box_model.py
@@ -1,11 +1,12 @@
 """
+Authors : Toufik FERHAT, Asma DAGMOUNE
 This programm is used to create a class that represents a black box model using tensorflox framework. :
     - The model conssists of  CNN , Fully connected layer and a softmax layer :
     - The CNN is used to extract features from spectrogram images and it contains :
         - Convolutional layer with 32 filters and kernel size of 12x8
         - Max pooling layer with kernel size of 4x1
     - The output of CNN is flattened and fed into a fully connected layer with 200 neurons
-    - The output of fully connected layer is fed into a softmax layer with 8 neurons
+    - The output of fully connected layer is fed into a softmax layer with 10 neurons
 """
 
 # Importing the required libraries
@@ -13,107 +14,167 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import Model
 from fetch_training_dataset import fetch_spectogram_dataset
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import itertools
+import argparse
 
-
-# Creating a class that represents a black box model
-class BlackBoxModel(Model):
-    """
-    This class is used to create a black box model using tensorflow framework.
-    """
-
+# Defining the black box model
+class BlackBoxModel:
     def __init__(self):
-        """
-        This function is used to create a black box model.
-        """
-        super(BlackBoxModel, self).__init__()
-        self.conv1 = tf.keras.layers.Conv2D(32, (12, 8), activation='relu')
-        self.pool1 = tf.keras.layers.MaxPool2D((4, 1))
-        self.flatten = tf.keras.layers.Flatten()
-        self.dense1 = tf.keras.layers.Dense(200, activation='relu')
-        self.dense2 = tf.keras.layers.Dense(8, activation='softmax')
-
-    def call(self, x):
-        """
-        This function is used to call the model.
-        :param x: Input data
-        :return: Output of the model
-        """
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.flatten(x)
-        x = self.dense1(x)
-        x = self.dense2(x)
-        return x
-
-    def predict(self, x):
-        """
-        This function is used to predict the output of the model.
-        :param x: Input data
-        :return: Output of the model
-        """
-        return self(x)
+        self.model = None
+        self.model_name = "black_box_model"
+        self.model_path = "models/"+ self.model_name + self.model_name + ".h5"
+        self.model_weights_path = "models/" + self.model_name + "_weights.h5"
+        self.model_history_path = "models/" + self.model_name + "_history.png"
+        self.model_history = None
     
-    def loss(self, x, y):
+    def build_model(self):
         """
-        This function is used to calculate the loss of the model.
-        :param x: Input data
-        :param y: Output data
-        :return: Loss of the model
+        This function is used to create the black box model :
         """
-        return tf.keras.losses.categorical_crossentropy(y, self(x))
+        # Defining the input layer
+        input_layer = tf.keras.layers.Input(shape=(40, 80, 3))
+        
+        # Defining the convolutional layer
+        conv_layer = tf.keras.layers.Conv2D(32, (12, 8), activation='relu')(input_layer)
+        max_pool_layer = tf.keras.layers.MaxPool2D((4, 1))(conv_layer)
+        
+        # Defining the fully connected layer
+        fully_connected_layer = tf.keras.layers.Flatten()(max_pool_layer)
+        fully_connected_layer = tf.keras.layers.Dense(200, activation='relu')(fully_connected_layer)
+        
+        # Defining the output layer
+        output_layer = tf.keras.layers.Dense(10, activation='softmax')(fully_connected_layer)
+        
+        # Compiling the model
+        self.model = Model(input_layer, output_layer)
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        
+    def train_model(self, train_generator, epochs=10):
+        """
+        This function is used to train the black box model :
+        """
+        # Training the model
+        self.model_history = self.model.fit_generator(train_generator, epochs=epochs)
+        
+    def save_model(self):
+        """
+        This function is used to save the black box model :
+        """
+        # Save the model
+        self.model.save(self.model_path)
+        
+    def load_model(self):
+        """
+        This function is used to load the black box model :
+        """
+        # Load the model
+        self.model = tf.keras.models.load_model(self.model_path)
+        
+    def save_model_weights(self):
+        """
+        This function is used to save the weights of the black box model :
+        """
+        # Save the weights
+        self.model.save_weights(self.model_weights_path)
     
-    def accuracy(self, x, y):
-        """
-        This function is used to calculate the accuracy of the model.
-        :param x: Input data
-        :param y: Output data
-        :return: Accuracy of the model
-        """
-        return tf.keras.metrics.categorical_accuracy(y, self(x))
-    
-    def train(self, train_generator, epochs=10, batch_size=32, verbose=True):
-        """
-        This function is used to train the model.
-        :param train_generator: Training data
-        :param epochs: Number of epochs
-        :param batch_size: Batch size
-        :param verbose: Verbose
-        :return: None
-        """
-        self.compile(optimizer='adam', loss=self.loss, metrics=[self.accuracy])
-        self.fit_generator(train_generator, epochs=epochs, steps_per_epoch=len(train_generator),
-                           verbose=verbose, use_multiprocessing=True, workers=4)
-    
-    def evaluate(self, test_generator, batch_size=32):
-        """
-        This function is used to evaluate the model.
-        :param test_generator: Test data
-        :param batch_size: Batch size
-        :return: None
-        """
-        return self.evaluate_generator(test_generator, steps=len(test_generator),
-                                       use_multiprocessing=True, workers=4, verbose=1)
 
-
-# Main --------------------------------------------------------------------------------------------------------------------
-if __name__ == "__main__":
+    def evaluate_model(self, test_generator):
+        """
+        This function is used to evaluate the black box model :
+        """
+        # Evaluate the model
+        test_loss, test_acc = self.model.evaluate_generator(test_generator)
+        print("Test loss: ", test_loss)
+        print("Test accuracy: ", test_acc)
     
-    # Load the training data
-    train_generator = fetch_spectogram_dataset('data/dataset')
+    def plot_model_history(self):
+        """
+        This function is used to plot the model history :
+        """
+        # Plot the model history
+        plt.plot(self.model_history.history['loss'])
+        plt.plot(self.model_history.history['accuracy'])
+        plt.title('Model accuracy')
+        plt.ylabel('Accuracy')
+        plt.xlabel('Epoch')
+        plt.legend(['Loss', 'Accuracy'], loc='upper left')
+        plt.savefig(self.model_history_path)
+        plt.show()
+        
+    def predict_class(self, spectrogram_image):
+        """
+        This function is used to predict the class of the spectrogram image :
+        """
+        # Predict the class
+        prediction = self.model.predict(spectrogram_image)
+        return np.argmax(prediction)
 
-    # Instantiate the model
+    def predict_classes(self, spectrogram_images):
+        """
+        This function is used to predict the classes of the spectrogram images :
+        """
+        # Predict the classes
+        predictions = self.model.predict(spectrogram_images)
+        return np.argmax(predictions, axis=1)
+
+    
+    def plot_confusion_matrix(self, test_generator, classes=None):
+        """
+        This function is used to plot the confusion matrix :
+        """
+        # Predict the classes
+        predictions = self.predict_classes(test_generator)
+        # Plot the confusion matrix
+        cm = confusion_matrix(test_generator.classes, predictions)
+        if classes is None:
+            classes = test_generator.class_indices.keys()
+        plt.figure()
+        plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+        plt.title("Confusion matrix")
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+        fmt = 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.tight_layout()
+        plt.savefig("models/"+ self.model_name +"confusion_matrix.png")
+        plt.show()
+
+def main():
+    
+    # Fetch the training and test dataset
+    train_generator = fetch_spectogram_dataset("data/train")
+    test_generator =  fetch_spectogram_dataset("data/test")
+    
+    # Create the black box model
     black_box_model = BlackBoxModel()
+    
+    # Build the black box model
+    black_box_model.build_model()
+    
+    # Train the black box model
+    black_box_model.train_model(train_generator, epochs=50)
 
-    # Train the model
-    black_box_model.train(train_generator, epochs=10, batch_size=32, verbose=True)
+    # Save the black box model
+    black_box_model.save_model()
 
-    # Evaluate the model
-    loss, accuracy = black_box_model.evaluate(train_generator, batch_size=32)
+    # Evaluate the black box model
+    black_box_model.evaluate_model(test_generator)
 
-    # Print the accuracy
-    print('Accuracy: ', accuracy)
+    # Plot the model history
+    black_box_model.plot_model_history()
+    
+    # Plot the confusion matrix
+    black_box_model.plot_confusion_matrix(test_generator)
 
-    # Save the model
-    #black_box_model.save('models/black_box_model.h5')
-
+if __name__ == "__main__":
+    main()
+        
 
