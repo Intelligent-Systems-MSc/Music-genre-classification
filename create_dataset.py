d640e8e4e133298cdd9f1f7ab0b9006c618eb728
Toufik <ferhattoufik@outlook.fr>
2021-11-27 19:16:19 +0000

    Update create dataset, added new features extraction mfcc

---
+++ create_dataset.py
@@ -0,0 +1,105 @@
+#Import needed libraries
+import os
+import numpy as np
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import argparse
+
+
+"""
+Authors: Toufik FERHAT, Asma DAGMOUNE , Rayane KADEM
+This script is used to create audio features dataset : 
+    - Mel-spectrogram
+    - MFCC
+    - Chroma-stft
+    - Chroma-cqt
+    - Chroma-cens
+
+The audio files dataset consists of 10 folders which correspond to 10 different genres and each folder contains 100 different audio files.
+According to the choosed feature, it will create a dataset of images representing the audio files.
+"""
+
+def create_dataset(dataset_path_in, dataset_path_out, feature, n_fft, hop_length):
+    """
+    This function is used to create dataset of images.
+    :param dataset_path_in: path of the dataset
+    :param dataset_path_out : path of image dataset
+    :param feature: feature to extract
+    :param n_fft: number of fft points
+    :param hop_length: number of samples between successive frames
+    :return:
+    """
+    # Create the folders
+    if not os.path.exists(dataset_path_out):
+        os.makedirs(dataset_path_out)
+    if not os.path.exists(dataset_path_out + "/" + feature):
+        os.makedirs(dataset_path_out + "/" + feature)
+
+    # Get the list of folders
+    folders = os.listdir(dataset_path_in)
+    # For each folder
+    for folder in tqdm(folders):
+        # Get the list of audio files
+        files = os.listdir(dataset_path_in + "/" + folder)
+        # For each audio file
+        for file in tqdm(files):
+            # Get the path of the audio file
+            path = dataset_path_in + "/" + folder + "/" + file
+            # Read the audio file
+            y, sr = librosa.load(path)
+            # Extract the feature
+            if feature == "melspectrogram":
+                image = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length)
+            elif feature == "mfcc":
+                image = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+            elif feature == "chroma_stft":
+                image = librosa.feature.chroma_stft(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length)
+            elif feature == "chroma_cqt":
+                image = librosa.feature.chroma_cqt(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length)
+            elif feature == "chroma_cens":
+                image = librosa.feature.chroma_cens(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length)
+            
+            # Ommit the .wav file extension file
+            file = file[:-4]
+
+            # Check if the folder exists
+            if not os.path.exists(dataset_path_out + "/" + feature + "/" + folder):
+                os.makedirs(dataset_path_out + "/" + feature + "/" + folder)
+            
+            # Librosa display 
+            librosa.display.specshow(librosa.power_to_db(image, ref = np.max), sr =sr , hop_length = hop_length)
+            # Ommit axis  
+            plt.axis('off')
+            plt.axis('tight')
+            # Set padding to 0
+            plt.tight_layout(pad=0)
+                 
+            # Save the image
+            plt.savefig(dataset_path_out + "/" + feature + "/" + folder + "/" + file + ".png")
+            # Close the plot
+            plt.close()
+            
+
+
+        
+def main():
+    
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Create the dataset of images")
+    # Add the arguments
+    parser.add_argument("--dataset_path_in", type=str, default="data/genres" ,help="Path of the dataset")
+    parser.add_argument("--dataset_path_out", type=str, default="data/dataset" ,help="Path of the dataset")
+    parser.add_argument("--feature", type=str,default= "melspectrogram" ,help="Feature to extract : melspectrogram, mfcc, chroma_stft, chroma_cqt, chroma_cens")
+    parser.add_argument("--n_fft", type=int, default = 1024,help="Number of fft points")
+    parser.add_argument("--hop_length", type=int, default = 512,help="Number of samples between successive frames")
+
+    # Parse the arguments
+    args = parser.parse_args()
+    # Create the dataset
+    create_dataset(args.dataset_path_in, args.dataset_path_out, args.feature, args.n_fft, args.hop_length)
+
+
+if __name__ == "__main__":
+    main()
--- split_dataset.py
+++ split_dataset.py
@@ -1,5 +1,7 @@
 """
 Author : Toufik FERHAT, Asma DAGMOUNE
+
+The original dataset is formed of 10 folders which correspends to 10 classes, each folder contains 100 audio files
 This program is used to split the dataset into training and testing dataset.
 """
 
@@ -8,55 +10,70 @@ import os
 import numpy as np
 import argparse
 
- # Split the dataset in training (80%) and testing (20%)
-def split_dataset(dataset_path, train_size=0.7,validation_size=0.8):
-    # Iterate over the directories in the folder "dataset"
-    i = 0 #count for elements
-    for dir in os.listdir(dataset_path):
-        i = 0 #count for elements
-        # For each directory, iterate over the spectogram files
-        for file in os.listdir(dataset_path + "/" + dir):
-            # Get the absolute path of the file for windows and linux
-            file_path = os.path.abspath(dataset_path + "/" + dir + "/" + file)
-            # IF windows OS is used
-            if os.name == 'nt':
-                #Delete the double backslash in the path
-                file_path = file_path.replace("\\", "/")
-    
-            # Check if data/train, validation and test directories exist
-            if not os.path.exists("data/train/" + dir):
-                # Create the directory if it doesn't exist
-                os.makedirs("data/train/" + dir)
-            if not os.path.exists("data/test/" + dir):
-                # Create the directory if it doesn't exist
-                os.makedirs("data/test/" + dir)
-            if not os.path.exists("data/validation/" + dir):
-                # Create the directory if it doesn't exist
-                os.makedirs("data/validation/" + dir)
-            
-             # Split the dataset in training and testing
-            if i < train_size*100:
-                # Move the file to the folder "train"
-                os.rename(file_path, os.path.abspath("data/train/" + dir + "/" + file))
-            elif( train_size*100 <= i < validation_size*100):
-                #move the file to the folder "validation"
-                os.rename(file_path, os.path.abspath("data/validation/" + dir + "/" + file))
-            else : 
-                # Move the file to the folder "test"
-                os.rename(file_path, os.path.abspath("data/test/" + dir + "/" + file))
-            i+=1
 
+def split_dataset(dataset_path_in, path_out, train_size, validation_size):
+    """
+    Split the dataset into training , validation and testing set
+    """
+    #  Get the list of all the folders
+    folders = os.listdir(dataset_path_in)
+    
+    # Create the output folders
+    if not os.path.exists(path_out):
+        os.makedirs(path_out)
+    if not os.path.exists(path_out + "/train"):
+        os.makedirs(path_out + "/train")
+    if not os.path.exists(path_out + "/validation"):
+        os.makedirs(path_out + "/validation")
+    if not os.path.exists(path_out + "/test"):
+        os.makedirs(path_out + "/test")
+
+    for folder in folders:
+        # Get the list of all the files in the folder
+        files = os.listdir(dataset_path_in + "/" + folder)
+        # Get the number of files in the folder
+        nb_files = len(files)
+        # Get the number of files in the training set
+        nb_train_files = int(nb_files * train_size)
+        # Get the number of files in the validation set
+        nb_validation_files = int(nb_files * validation_size)
+        # Get the number of files in the testing set
+        nb_test_files = nb_files - nb_train_files - nb_validation_files
+        
+        # Create the training set
+        for i in range(nb_train_files):
+            # Check if path exists
+            if not os.path.exists(path_out + "/train/" + folder):
+                os.makedirs(path_out + "/train/" + folder)
+            os.rename(dataset_path_in + "/" + folder + "/" + files[i], path_out + "/train/" + folder + "/" + files[i])
+        # Create the validation set
+        for i in range(nb_train_files, nb_train_files + nb_validation_files):
+            # Check if path exists
+            if not os.path.exists(path_out + "/validation/" + folder):
+                os.makedirs(path_out + "/validation/" + folder)
+            os.rename(dataset_path_in + "/" + folder + "/" + files[i], path_out + "/validation/" + folder + "/" + files[i])
+        # Create the testing set
+        for i in range(nb_train_files + nb_validation_files, nb_files):
+            # Check if path exists
+            if not os.path.exists(path_out + "/test/" + folder):
+                os.makedirs(path_out + "/test/" + folder)
+            os.rename(dataset_path_in + "/" + folder + "/" + files[i], path_out + "/test/" + folder + "/" + files[i])
+        
+
+    
+    
 def main():
     # Parse the arguments
-    parser = argparse.ArgumentParser(description="Split the dataset into training and testing dataset")
-    parser.add_argument("--dataset_path", type=str, default="data/dataset", help="Path to the dataset")
-    parser.add_argument("--train_size", type=float, default=0.7, help="Train size")
-    parser.add_argument("--validation_size", type=float, default=0.1, help="Validation size")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_path", type=str, default="data/dataset/melspectrogram/", help="Path to the dataset")
+    parser.add_argument("--path_out", type=str, default="data/images/melspectorgram/", help="Path to the output folder")
+    parser.add_argument("--train_size", type=float, default=0.8, help="Size of the training set")
+    parser.add_argument("--validation_size", type=float, default=0, help="Size of the validation set")
     args = parser.parse_args()
 
     # Split the dataset
     try :
-        split_dataset(args.dataset_path, args.train_size, args.validation_size)
+        split_dataset(args.dataset_path, args.path_out,args.train_size, args.validation_size)
     except Exception as e:
         print(e)
     
